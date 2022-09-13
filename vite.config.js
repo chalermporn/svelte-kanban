63206f0a1461964927f2ea77549567971fec37d4
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2022-09-14 00:07:32 +0700

    fix: deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path'
+import { hostname } from 'os'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    https: true
+  },
   optimizeDeps: {
     exclude: ['tinro']
   },
