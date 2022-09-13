1d4c8fb06fbc5c807fe085ac85575f49d35c7753
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2022-09-14 00:12:26 +0700

    fix: host

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path'
-import { hostname } from 'os'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "0.0.0.0",
-    https: true
+    // https: true,
+    port: 80
   },
   optimizeDeps: {
     exclude: ['tinro']
